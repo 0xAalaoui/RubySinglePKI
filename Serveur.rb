22f711f83c1b9c65511a81bcefdcd200240e832a
Ali Aalaoui <ali@aalaoui.com>
2016-03-20 13:42:05 +0100

    Update Serveur.rb

--- Serveur.rb
+++ Serveur.rb
@@ -16,32 +16,23 @@ socket = TCPServer.new('localhost', 2001)
 keys = OpenSSL::PKey::RSA.new(File.open("End_Entity/Serveur.key"))
 
 if not File.exist?('End_Entity/Serveur.crt')
-	
-	randomAESkey 			= $cipher.random_key	
-	root_ca 	 			= OpenSSL::X509::Certificate.new(File.open("CA/CA.crt"))
-	PubKey_CA				= root_ca.public_key
+	randomAESkey 		= $cipher.random_key	
+	root_ca 	 	= OpenSSL::X509::Certificate.new(File.open("CA/CA.crt"))
+	PubKey_CA		= root_ca.public_key
 	aeskeyEncWithPubKeyCA	= PubKey_CA.public_encrypt(randomAESkey)
 	aeskeyEncWithPubKeyCA 	= Base64.encode64(aeskeyEncWithPubKeyCA)
-
-	socketWithCA 			= TCPSocket.new('localhost', 3000)
+	socketWithCA 		= TCPSocket.new('localhost', 3000)
 	puts "[SERVER] Waiting for certificate"
-	
 	socketWithCA.write aeskeyEncWithPubKeyCA #AES key encrypted with PubKey CA
-	
 	message = socketWithCA.recv(512) #Message from CA
 	puts message
-	
 	pubKey = keys.public_key.to_s
 	pubKeyEncWithAES = encryption(pubKey, randomAESkey) #PubKey encrypted with AES
-	
 	puts "[SERVER] Sending my encrypted public key"
 	socketWithCA.write pubKeyEncWithAES
-
 	certificate = socketWithCA.recv(2048) #Certificate from CA 				
 	certificate = OpenSSL::X509::Certificate.new(certificate) 
-	
 	puts "[SERVER] Storing certificate"
-	
 	File.open 'End_Entity/Serveur.crt', 'w' do |io| io.write certificate.to_pem end
 end
 
@@ -49,9 +40,9 @@ end
 ssl_context 			= OpenSSL::SSL::SSLContext.new()
 ssl_context.cert 		= OpenSSL::X509::Certificate.new(File.open("End_Entity/Serveur.crt"))
 ssl_context.key 		= keys
-ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER|OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
+ssl_context.verify_mode 	= OpenSSL::SSL::VERIFY_PEER|OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
 ssl_context.ca_file		= 'CA/CA.crt'
-ssl_socket 				= OpenSSL::SSL::SSLServer.new(socket, ssl_context)
+ssl_socket 			= OpenSSL::SSL::SSLServer.new(socket, ssl_context)
 
 
 puts "[SERVER] Waiting for clients"
