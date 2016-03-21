19450ddc477dfe7aef09b8cbb434c5af98f94321
Ali Aalaoui <ali@aalaoui.com>
2016-03-21 20:53:09 +0100

    Update Client1.rb

--- Client1.rb
+++ Client1.rb
@@ -3,12 +3,12 @@ require 'openssl'
 
 socket = TCPSocket.new('localhost', 2001)
 
-ssl_context 			= OpenSSL::SSL::SSLContext.new
-ssl_context.cert 		= OpenSSL::X509::Certificate.new(File.open("End_Entity/Client1.crt"))
-ssl_context.key 		= OpenSSL::PKey::RSA.new(File.open("End_Entity/Client1.key"))
+ssl_context 			      = OpenSSL::SSL::SSLContext.new
+ssl_context.cert 		    = OpenSSL::X509::Certificate.new(File.open("End_Entity/Client1.crt"))
+ssl_context.key 		    = OpenSSL::PKey::RSA.new(File.open("End_Entity/Client1.key"))
 ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER | OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
-ssl_context.ca_file		= 'CA/CA.crt'
-ssl_socket 				= OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
+ssl_context.ca_file		  = 'CA/CA.crt'
+ssl_socket 				      = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
 
 ssl_socket.connect
 
